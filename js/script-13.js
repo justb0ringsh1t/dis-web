bf81c3fcaeb20fc54f3947af50026b4ef5d7ca7b
Santiago Garcia <just.b000rinsh1t@gmail.com>
2022-04-09 18:34:22 -0300

    Mejora para el responsive

--- js/script-13.js
+++ js/script-13.js
@@ -1,25 +1,28 @@
 $('document').ready(() => {
-  $('#btnaceptar').on('click', () => {
+  $('#btn-calcular').on('click', () => {
     const asignatura = $('#asignatura').val()
     const oral = Number($('#oral').val())
     const practico = Number($('#practico').val())
     const escrito = Number($('#escrito').val())
 
     const promedio = Math.round((oral + practico + escrito) / 3)
+
     let juicio
     let parrafoCorrespondiente
 
-    if(promedio < 4){
-        juicio = 'Examen Febrero'
-        parrafoCorrespondiente = '#febrero'
-    }else if (promedio >= 4 && promedio < 8){
-        juicio = 'Examen Diciembre'
-        parrafoCorrespondiente = '#diciembre'
-    }else{
-        juicio = 'Exonerado'
-        parrafoCorrespondiente = '#exonera'
+    if (promedio < 4) {
+      juicio = 'Examen Febrero'
+      parrafoCorrespondiente = '#febrero'
+    } else if (promedio >= 4 && promedio < 8) {
+      juicio = 'Examen Diciembre'
+      parrafoCorrespondiente = '#diciembre'
+    } else {
+      juicio = 'Exonerado'
+      parrafoCorrespondiente = '#exonero'
     }
 
-    $(parrafoCorrespondiente).append(`${asignatura} ${promedio} ${juicio} <br />`)
+    $(parrafoCorrespondiente).append(
+      `${asignatura} ${promedio} ${juicio} <br />`
+    )
   })
 })
