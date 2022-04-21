88c38b89a9712cc5b6002686a186eeeb1f51222f
Santiago Garcia <just.b000rinsh1t@gmail.com>
2022-04-21 14:45:05 -0300

    Deber 2

---
+++ js/script-deber-2.js
@@ -0,0 +1,26 @@
+$(() => {
+  $('#boton').on('click', () => {
+    const num1 = Number($('#num1').val())
+    const num2 = Number($('#num2').val())
+    const multiplo = Number($('#num3').val())
+    const parrafo = $('#resultado')
+
+    parrafo.html('')
+
+    if (num1 === '' || num2 === '' || multiplo === '') {
+      parrafo.html('Complete todos los campos')
+      return
+    }
+
+    if (multiplo === 0) {
+      parrafo.html('Error, no hay multiplos de 0')
+    }
+
+    const menor = num1 <= num2 ? num1 : num2
+    const mayor = num2 >= num1 ? num2 : num1
+
+    for (let i = menor; i <= mayor; i++) {
+      parrafo.append(`${i % multiplo === 0 ? i : ''} `)
+    }
+  })
+})
